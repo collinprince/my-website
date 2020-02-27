ac3ea98d637947af1ef71c8ca3f33db90907fbd3
Collin Prince <collin@Collins-MacBook-Pro-2.local>
2020-02-26 20:38:22 -0800

    Updated colors, adding new JS features

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, HostListener } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { PageScrollService } from 'ngx-page-scroll-core';
 import { HomeComponent } from '../home/home.component';
@@ -8,6 +8,7 @@ import { HomeComponent } from '../home/home.component';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
+//
 
 
 export class NavbarComponent implements OnInit {
@@ -21,11 +22,33 @@ export class NavbarComponent implements OnInit {
     this.collapsed = !this.collapsed;
   }
 
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onWindowScroll() {
+      let elem = document.getElementById('bar');
+      elem.setAttribute("style", "opacity: 1; transition-duration: .25s;");
+      setTimeout(function() {
+        elem.setAttribute("style", "opacity: 0; transition-duration: .25s;");
+	    }, 1000);
+  }
+
+
   scrollToElement(id: string): void {
       let elem = document.getElementById(id);
       console.log(elem);
       elem.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     }
+
+  onHover() {
+    let elem = document.getElementById('bar');
+    elem.setAttribute("style", "opacity: 1; transition-duration: .25s;");
+  }
+
+  onExit() {
+    let elem = document.getElementById('bar');
+    elem.setAttribute("style", "opacity: 0; transition-duration: .25s;");
+
+  }
+
 
   homeScroll() {
     if(this.home.home)
